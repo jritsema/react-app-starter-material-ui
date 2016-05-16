0ad326c84a681e0d51aee29ad767df0eaf2e1fd3
John Ritsema <john_ritsema@yahoo.com>
2016-05-16 16:43:41 -0400

    reverted color palette back to defaults

--- components/app.jsx
+++ components/app.jsx
@@ -15,8 +15,20 @@ import { withRouter } from 'react-router'
 
 const muiTheme = getMuiTheme({
   palette: {
-    primary1Color: blue500,
-    accent1Color: teal500,
+    // primary1Color: cyan500,
+    // primary2Color: cyan700,
+    // primary3Color: grey400,
+    // accent1Color: pinkA200,
+    // accent2Color: grey100,
+    // accent3Color: grey500,
+    // textColor: darkBlack,
+    // alternateTextColor: white,
+    // canvasColor: white,
+    // borderColor: grey300,
+    // disabledColor: fade(darkBlack, 0.3),
+    // pickerHeaderColor: cyan500,
+    // clockCircleColor: fade(darkBlack, 0.07),
+    // shadowColor: fullBlack,
   },
 });
 
